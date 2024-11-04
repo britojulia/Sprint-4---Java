00c038d8e46141e23e4fbad33d9c64c34573f25a
britojulia <juliabri1234@gmail.com>
2024-11-04 15:52:27 -0300

    finalizando ultimos ajustes

--- src/main/java/br/com/fiap/resource/CorsFilter.java
+++ src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -1,10 +1,10 @@
 package br.com.fiap.resource;
 
-import jakarta.ws.rs.OPTIONS;
+
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.core.Response;
+
 
 import java.io.IOException;
 
@@ -13,13 +13,8 @@ public class CorsFilter implements ContainerResponseFilter {
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        request.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        request.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    }
-
-    @OPTIONS
-    public Response options() {
-        return Response.ok().build();
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
 
