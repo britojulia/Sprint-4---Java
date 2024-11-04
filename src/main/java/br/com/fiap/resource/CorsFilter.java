9aa917b31b3773135c66c0379ff3ec6ae6b4ad5c
britojulia <juliabri1234@gmail.com>
2024-11-04 20:44:28 -0300

    finalizando

--- src/main/java/br/com/fiap/resource/CorsFilter.java
+++ src/main/java/br/com/fiap/resource/CorsFilter.java
@@ -4,10 +4,12 @@ package br.com.fiap.resource;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
 
 
 import java.io.IOException;
 
+@Provider
 public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
